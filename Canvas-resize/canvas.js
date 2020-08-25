67517c2f621642defcf4b11200fbfb67c18dc590
Hồ Hải Thủy <HHT.ThuyHH@gmail.com>
2020-08-25 14:41:55 +0700

    Finish canvas tutorial

--- Canvas-resize/canvas.js
+++ Canvas-resize/canvas.js
@@ -1,12 +1,3 @@
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
-}
-
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -14,13 +5,69 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d')
 
 // ========================================================================================================
-function Circle(x, y, radius, dx, dy, color) {
+
+
+var mouse = {
+    x: undefined,
+    y: undefined
+
+}
+
+window.addEventListener('mousemove',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+)
+
+window.addEventListener('resize',
+    function(){
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        init();
+    }
+)
+var colorArray = [
+    '#F25CA2',
+    '#0433BF',
+    '#032CA6', 
+    '#021859',
+    '#0B9ED9',
+];  
+var circleArray = [];
+init();
+console.log();
+animate();
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    for (i = 0; i < circleArray.length; i++)
+        circleArray[i].move();
+    // interactivity
+
+}
+
+function init(){
+    circleArray = [];
+    for (var i = 0; i < 250; i++) {
+        var radius = 50;
+        var x = Math.random() * (window.innerWidth - 2 * radius) + radius;
+        var y = Math.random() * (window.innerHeight - 2 * radius) + radius;
+        var dx = Math.random() * 4;
+        var dy = Math.random() * 4;
+        circleArray.push(new Circle(x, y, radius, dx, dy));
+    }
+}
+
+function Circle(x, y, radius, dx, dy) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.dx = dx;
     this.dy = dy;
-    this.color = color;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
     this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -39,46 +86,13 @@ function Circle(x, y, radius, dx, dy, color) {
         }
         this.x += this.dx;
         this.y += this.dy;
-
+        if(mouse.x - this.x < 30 && mouse.x - this.x > -30 && mouse.y - this.y < 30 && mouse.y - this.y > -30){
+            if(this.radius < 80)
+                this.radius += 3;
+        }
+        else if(this.radius > 5){
+            this.radius -= 1;
+        }
         this.draw();
     }
-}
-
-var mouse = {
-    x: undefined,
-    y: undefined
-
-}
-
-window.addEventListener('mousemove',
-    function (event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-    }
-)
-
-
-var circleArray = [];
-
-for (var i = 0; i < 50; i++) {
-    var radius = 50;
-    var x = Math.random() * (window.innerWidth - 2 * radius) + radius;
-    var y = Math.random() * (window.innerHeight - 2 * radius) + radius;
-    var dx = Math.random() * 4;
-    var dy = Math.random() * 4;
-    var color = getRandomColor();
-    circleArray.push(new Circle(x, y, radius, dx, dy, color));
-}
-
-
-animate();
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    for (i = 0; i < circleArray.length; i++)
-        circleArray[i].move();
-    // interactivity
-
 }
