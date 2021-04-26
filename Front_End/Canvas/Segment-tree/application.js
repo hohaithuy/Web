d48634b7b83d44af00242098a3b1fe09a471e7a0
Hồ Hải Thủy <HHT.ThuyHH@gmail.com>
2021-04-26 18:08:33 +0700

    Options

---
+++ Front_End/Canvas/Segment-tree/application.js
@@ -0,0 +1,11 @@
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = 500;
+canvas.height = 500;
+
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
+console.log(canvas);
